960ac352c4b3e482226cfc858737925fa1bb2c52
Kanstantsin Taukach <tovkachkot@gmail.com>
2024-01-11 23:30:46 +0300

    Lesson 4 - Components, FTransform type.

--- Source/MyGeometrySandbox/Private/BaseGeometryActor.cpp
+++ Source/MyGeometrySandbox/Private/BaseGeometryActor.cpp
@@ -14,6 +14,9 @@ ABaseGeometryActor::ABaseGeometryActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>("BaseMesh");
+	SetRootComponent(BaseMesh);
 
 }
 
@@ -22,9 +25,11 @@ void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-
+	InitialLocation = GetActorLocation();
+		
+	//printTransform();
 	//printStringTypes();
-	printTypes();
+	//printTypes();
 
 }
 
@@ -33,14 +38,21 @@ void ABaseGeometryActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// z = z0 + amplitude * sin(freq * t);
+	FVector CurrentLocation = GetActorLocation();
+	float time = GetWorld()->GetTimeSeconds();
+	CurrentLocation.Z = InitialLocation.Z + Amplitude * FMath::Sin(Frequency * time);
+
+	SetActorLocation(CurrentLocation);
+
 }
 
 // Lesson 1 (UE_LOG)
 void ABaseGeometryActor::printTypes() const
 {
-	UE_LOG(LogTemp, Display, TEXT("Hello UE!"));
-	UE_LOG(LogTemp, Warning, TEXT("Hello UE!"));
-	UE_LOG(LogTemp, Error, TEXT("Hello UE!"));
+//	UE_LOG(LogTemp, Display, TEXT("Hello UE!"));
+//	UE_LOG(LogTemp, Warning, TEXT("Hello UE!"));
+//	UE_LOG(LogTemp, Error, TEXT("Hello UE!"));
 
 
 	UE_LOG(LogBaseGeometry, Warning, TEXT("Actor name %s"), *GetName());
@@ -52,7 +64,7 @@ void ABaseGeometryActor::printTypes() const
 }
 
 // Lesson 2 ((FString)
-void ABaseGeometryActor::printStringTypes()
+void ABaseGeometryActor::printStringTypes() const
 {
 
 	FString Name = "John Connor";
@@ -67,5 +79,21 @@ void ABaseGeometryActor::printStringTypes()
 
 	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Cyan, Name);
 	GEngine->AddOnScreenDebugMessage(-1, 7.0f, FColor::Emerald, Stat, true, FVector2D(1.5f, 1.5f));
-};
+}
 
+// Lesson 3 (Компоненты. Тип FTransform)
+void ABaseGeometryActor::printTransform() const
+{
+	FTransform Transform = GetActorTransform();
+	FVector Location = Transform.GetLocation();
+	FRotator Rotation = Transform.Rotator();
+	FVector Scale = Transform.GetScale3D();
+
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Actor name %s"), *GetName());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Transform %s"), *Transform.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Location %s"), *Location.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Rotation %s"), *Rotation.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Scale %s"), *Scale.ToString());
+
+	UE_LOG(LogBaseGeometry, Error, TEXT("Human transform %s"), *Transform.ToHumanReadableString());
+}
--- Source/MyGeometrySandbox/Public/BaseGeometryActor.h
+++ Source/MyGeometrySandbox/Public/BaseGeometryActor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
 #include "BaseGeometryActor.generated.h"
 
 UCLASS()
@@ -15,9 +16,21 @@ public:
 	// Sets default values for this actor's properties
 	ABaseGeometryActor();
 
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	// Lesson 4
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Amplitude = 50.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Frequency = 2.0f;
+
 
 	// Lesson 3 (UPROPERTY - свойство)
 
@@ -41,7 +54,10 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 private:
+	FVector InitialLocation;
+
 	void printTypes() const;
-	void printStringTypes();
+	void printStringTypes() const;
+	void printTransform() const;
 
 };
